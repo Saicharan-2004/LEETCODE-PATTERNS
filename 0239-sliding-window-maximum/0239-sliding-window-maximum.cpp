ae8fa6d357aeb85852a98799ecab9ab57810b059
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-16 21:57:20 +0530

    Time: 217 ms (82.15%), Space: 134.7 MB (66.1%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,26 +2,44 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
     {
-        int start=0;
-        int end=k-1;
+        // int start=0;
+        // int end=k-1;
         int n=nums.size();
-        priority_queue<pair<int,int>>temp;
+        // priority_queue<pair<int,int>>temp;
         vector<int>ans;
-        // dequeue<int>temp;
-        for(int i=0;i<k;i++)
+        deque<int>temp;
+        for(int i=0;i<n;i++)
         {
-            temp.push({nums[i],i});
-        }
-        ans.push_back(temp.top().first);
-        for(int i=k;i<n;i++)
-        {
-            temp.push({nums[i],i});
-            while(temp.top().second<=i-k)
+            if(i-temp.front()==k)
             {
-                temp.pop();
+                temp.pop_front();
             }
-            ans.push_back(temp.top().first);
+            while(!temp.empty() && nums[temp.back()]<=nums[i])
+            {
+                temp.pop_back();
+            }
+            temp.push_back(i);
+            if(i>=k-1)
+            {
+                ans.push_back(nums[temp.front()]);
+            }
         }
+        // // dequeue<int>temp;
+        // for(int i=0;i<k;i++)
+        // {
+        //     temp.push({nums[i],i});
+        // }
+        // ans.push_back(temp.top().first);
+        // for(int i=k;i<n;i++)
+        // {
+        //     temp.push({nums[i],i});
+        //     while(temp.top().second<=i-k)
+        //     {
+        //         temp.pop();
+        //     }
+        //     ans.push_back(temp.top().first);
+        // }
+
         // int maxi=INT_MIN;
         // int maxi_ind=-1;
         // while(end<nums.size())
