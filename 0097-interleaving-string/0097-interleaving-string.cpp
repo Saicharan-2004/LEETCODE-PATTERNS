6cf1b92b5186d092392fd5724d8e19c6a856d667
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-25 09:22:50 +0530

    Time: 3 ms (78.62%), Space: 7 MB (63.23%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -33,7 +33,32 @@ public:
         int outlen=s3.length();
         if(n+m!=outlen)
             return false;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return helper(s1,s2,s3,0,0,0,n,m,outlen,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // return helper(s1,s2,s3,0,0,0,n,m,outlen,dp);
+        dp[n][m]=1;
+        for(int ind1=n;ind1>=0;ind1--)
+        {
+            for(int ind2=m;ind2>=0;ind2--)
+            {
+                // for(int ind3=outlen-1;ind3>=0;ind3--)
+                // {
+                    bool three=false;
+                    if(ind1<n && ind2<m && (s1[ind1]==s3[ind1+ind2] && s2[ind2]==s3[ind2+ind1]))
+                    {
+                        three=dp[ind1+1][ind2]||dp[ind1][ind2+1];
+                    }
+                    else if(ind1<n && s1[ind1]==s3[ind2+ind1])
+                    {
+                        three=dp[ind1+1][ind2];
+                    }
+                    else if(ind2<m && s2[ind2]==s3[ind1+ind2])
+                    {
+                        three=dp[ind1][ind2+1];
+                    }
+                    dp[ind1][ind2] = dp[ind1][ind2]||three;
+                // }
+            }
+        }
+        return dp[0][0]==1;
     }
 };
