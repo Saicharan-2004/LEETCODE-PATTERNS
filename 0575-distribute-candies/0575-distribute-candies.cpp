8f6512039596cd28d6971cceb7e214c65a1ff768
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-26 15:15:17 +0530

    Time: 222 ms (66.14%), Space: 110.4 MB (67.38%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) 
     {
-        set<int>temp(candyType.begin(),candyType.end());
+        unordered_set<int>temp(candyType.begin(),candyType.end());
         return min(temp.size(),candyType.size()/2);
     }
 };
